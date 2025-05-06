c5fe16c7bcb7421762c876616c781bc3b4d5371b
Geffs4 <200gonzaDESKTOP-NV3UU4G MINGW64>
2025-05-06 17:40:51 -0300

    ""

---
+++ src/TdaLista/TestListaTp4.java
@@ -0,0 +1,89 @@
+package TdaLista;
+
+import java.util.Iterator;
+
+
+
+public class TestListaTp4 {
+
+	public static void main(String[] args) {
+		ListaDoblementeEnlazada<Integer> l=new ListaDoblementeEnlazada<Integer>();
+		
+		for(int i=1;i<5;i++)
+			l.addLast(i);
+	
+		System.out.println(l.toString());
+		
+		
+//chequeo que belong funcione
+		boolean pertenece=true;
+		for(int i=1;i<5&&pertenece;i++) {
+			pertenece=belong(i,l);
+		}
+		String mensaje= (pertenece ? "Funciona":"no funciona");
+		System.out.println("belong:"+mensaje);	
+//---------------------------metodo: CantElements()------------------------------------------
+		ListaDoblementeEnlazada<String> A=new ListaDoblementeEnlazada<String>();
+		for(int i=1;i<10;i++)
+			A.addLast("a");
+		System.out.println(A.toString());
+		int cantE=cantElements("a",A);
+		mensaje=(cantE==9 ? "Funciona":"no funciona");
+		System.out.println("cantElements:"+mensaje);
+
+//---------------------------metodo cantBelong
+		mensaje=(!cantBelong(A,"a",3)? "Funciona":"no funciona");
+		System.out.println("cantBelong:"+mensaje);
+//---------------------------metodo: dupper
+		System.out.println(dupper(l).toString());
+	}
+	public static <E>boolean belong(E e1,PositionList<E>list) {
+		Iterator<E>it=list.iterator();
+		boolean encontre=false;
+		
+		while(it.hasNext()&&!encontre)
+			encontre=it.next().equals(e1);
+		
+		return encontre;
+	}
+	public static <E> int cantElements(E e1,PositionList<E>list) {
+		int Cont=0;
+		Iterator<E>it=list.iterator();
+		while(it.hasNext())
+			if(it.next().equals(e1))
+				Cont++;
+		
+		return Cont;
+	}
+	public static <E> boolean cantBelong(PositionList<E> list,E x,int n) {
+		int cant=0;
+		for(E elem:list)
+			if(elem.equals(x))
+				cant++;
+		
+		return cant==n;			
+	}
+	//tp4-4
+	public static <E> PositionList<E> dupper(PositionList<E> list){
+		PositionList<E>exit=new ListaDoblementeEnlazada<E>();
+		for(E aux:list) {
+			exit.addLast(aux);
+			exit.addLast(aux);
+			}
+		return exit;
+		}
+	public static <E> PositionList<E> removeLists(PositionList<E>l1,PositionList<E> l2){
+		if(l1.isEmpty()&&l2.isEmpty())
+			return l2;
+		PositionList<E>del=new ListaDoblementeEnlazada<E>();
+		
+		Position<E>p1=l1.first();
+		Position<E>p2=l2.first();
+		
+		E aux;
+		
+		while(p1!=null)
+			if(p1.equals(p2))
+				del.addLast(l2.remove(p2));
+		}
+}	
